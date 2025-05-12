b89ec4751afa35fd3b3adbc5dc489c6b5bea12a0
Leonard457 <laverovleonid442@gmail.com>
2025-05-12 23:29:20 +0300

    Добавьте файлы проекта.

---
+++ AppMvcMyTest_001/Data/ApplicationDbContext.cs.cs
@@ -0,0 +1,18 @@
+﻿// Data/ApplicationDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using AppMvcMyTest_001.Models; // Убедись, что пространство имен моделей правильное
+
+namespace AppMvcMyTest_001.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        // Добавь DbSet для каждой твоей модели (сущности)
+        // public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ AppMvcMyTest_001/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations; // Исправлено: добавлено "using" и ";" для корректного импорта пространства имен
+
+namespace AppMvcMyTest_001.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } // Первичный ключ (по соглашению)
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        // Исправлено: атрибуты валидации должны быть применены к свойствам
+        //[Required]
+        //[StringLength(100)]
+    }
+}
